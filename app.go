c3d917799a64b7fd27bf0ef53ebb5fbd8c6fd31c
akinfemi <akinfemi.aluko@gmail.com>
2017-10-07 08:36:08 -0700

    redis test code

--- app.go
+++ app.go
@@ -3,7 +3,45 @@ package main
 import (
 	"net/http"
     "fmt"
+    "github.com/go-redis/redis"
 )
+type ucred struct {
+    username string 
+    password string 
+}
+
+func NewClient(u *ucred) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	// pong, err := client.Ping().Result()
+	// fmt.Println(pong, err)
+    // Output: PONG <nil>
+    err := client.Set(u.username, u.password, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get(u.username).Result()
+	if err != nil {
+		panic(err)
+    }
+	fmt.Println(u.username, val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exists")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+    }
+    // Output: key value
+	// key2 does not exists
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[1:]
